e2ee4e15beb30350a92216bf649f4a9316522e8d
matiashteixeira <mateus.teixeira1@estudante.ufla.br>
2023-03-03 08:55:21 -0300

    A new era begins

--- include/types.h
+++ include/types.h
@@ -6,22 +6,8 @@
 #define MBIN1 PA7
 #define MBIN2 PB0
 
-#define INVERTER 0  //se quiser inverter os sensor colocar 1 se não 0
-// Linha branca INVERTER = 0, linha preta INVERTER = 1
-
-#define BORDA_RC 1
-#define BORDA_RC_TIMEOUT 350
-#define divisor 2 // divisor da faixa que determina o threshold
-
 #define LED_L1 PB12
 #define LED_L2 PA11
 #define LED_L3 PC13
 #define BOT1 PB3
 #define BOT2 PB4
-
-#define analogBat PA12   // Pino do leitor de bateria
-
-#define MPU_ADDR 0x68
-
-
-
