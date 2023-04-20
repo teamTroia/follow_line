5c9780dc918c43447f6bbccc576fe6aa5524f8d4
ana elis <ana.oliveira35@estudante.ufla.br>
2023-04-20 01:47:03 -0300

    bala 20-04-23

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,11 @@ uint16_t valores_borda[qtd_borda]; //Criação do vetor para armazenar os valore
 
 bool calibrado = 0, ligado = 0, parada = 1; //Indica se já foi calibrado e se ta ligado, respectivamente
 
-float Kp = 9, Kd = 10, Ki = 0; //Constantes multiplicativas para o PID
+float Kp = 12.5, Kd = 10, Ki = 0; //Constantes multiplicativas para o PID
 
 float I = 0, erro_anterior = 0;
-int velocidade = 110; //Velocidade para os motores (pode e deve ser ajustada)
-uint8_t velocidade_maxima = 130;
+int velocidade = 70; //Velocidade para os motores (pode e deve ser ajustada)
+uint8_t velocidade_maxima = 90;
 
 int erros[6] = {26, 16, 6, -6, -16, -26}; //Valores dos erros para cada situação de leitura dos sensores
 unsigned long int tempo_anterior = 0, tempo_anterior2 = 0, tempo_parada = 0;
